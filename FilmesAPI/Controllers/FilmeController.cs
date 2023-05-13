e0dc921447104c3cf0278b3baf4af4c2737bbd1e
GabrielMikami <gabrielmikami4@gmail.com>
2023-05-12 23:13:18 -0300

    commit aula 3.7

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,10 +8,12 @@ namespace FilmesAPI.Controllers;
 public class FilmeController
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Genero);
@@ -19,8 +21,14 @@ public class FilmeController
     }
 
     [HttpGet]
-    public List<Filme> RecuperaFilmes()
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
     {
-        return filmes;
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
