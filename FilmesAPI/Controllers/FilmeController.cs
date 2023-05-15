41e4edae67b0a750b2261669d1963fa56ffee132
GabrielMikami <gabrielmikami4@gmail.com>
2023-05-15 20:04:03 -0300

    commit aula 5.2

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -43,4 +43,21 @@ public class FilmeController : ControllerBase
         return Ok(filme);
     }
 
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+
 }
